768dd47b5303ce641b7cb80cd994a25ece06d258
septian <septianibnyohan@gmail.com>
2023-03-19 20:14:44 +0700

    Add entity models

---
+++ SolarCoffee.Data/Models/SalesOrderItem.cs
@@ -0,0 +1,9 @@
+namespace SolarCoffee.Data.Models
+{
+    public class SalesOrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public Product Product { get; set; }
+    }
+}
